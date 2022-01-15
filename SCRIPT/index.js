ca3e8a3401084f0dc139fda6085a313cfec416ec
yiching-w <doo8409255@gmail.com>
2022-01-15 16:59:00 +0800

    Feature: smoothScroll

--- SCRIPT/index.js
+++ SCRIPT/index.js
@@ -1,58 +1,81 @@
-$(document).ready(function() {
-    loadingWait();
+$(document).ready(function () {
+  loadingWait()
 
-    function loadingWait() {
-        setTimeout(() => {
-            $('#preloader').fadeOut();
-            $('BODY').css('overflow', 'auto');
-        }, 1300);
+  function loadingWait() {
+    setTimeout(() => {
+      $('#preloader').fadeOut()
+      $('BODY').css('overflow', 'auto')
+    }, 1300)
+  }
+
+  const textList = ['Wang Yi Ching', 'Front-End Developer']
+
+  typing(textList, $('#featureText'))
+
+  async function typing(textList, eleRef) {
+    let i = 0
+    while (true) {
+      await typeSentence(textList[i], eleRef)
+      await sleep(1500)
+      await deleteSentence(eleRef)
+      await sleep(500)
+      i++
+      if (i >= textList.length) {
+        i = 0
+      }
     }
+  }
 
-    const textList = ['Wang Yi Ching', 'Front-End Developer'];
-
-    typing(textList, $('#featureText'));
-
-    async function typing(textList, eleRef) {
-        let i = 0;
-        while (true) {
-            await typeSentence(textList[i], eleRef);
-            await sleep(1500);
-            await deleteSentence(eleRef);
-            await sleep(500);
-            i++;
-            if (i >= textList.length) { i = 0 };
-        }
+  async function typeSentence(text, eleRef) {
+    const letters = text.split('')
+    for (let letter of letters) {
+      await sleep(100)
+      eleRef.append(letter)
     }
+    return
+  }
 
-    async function typeSentence(text, eleRef) {
-        const letters = text.split('');
-        for (let letter of letters) {
-            await sleep(100);
-            eleRef.append(letter);
-        };
-        return;
+  async function deleteSentence(eleRef) {
+    let text = eleRef.html()
+    let textLength = text.length
+    while (textLength > 0) {
+      await sleep(100)
+      text = text.slice(0, -1)
+      eleRef.html(text)
+      textLength -= 1
     }
+  }
 
-    async function deleteSentence(eleRef) {
-        let text = eleRef.html()
-        let textLength = text.length;
-        while (textLength > 0) {
-            await sleep(100);
-            text = text.slice(0, -1);
-            eleRef.html(text);
-            textLength -= 1;
-        }
+  async function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
+
+  window.addEventListener('scroll', () => {
+    if ($(window).scrollTop() == '0') {
+      $('.bgImg').css('transform', 'translate(0px, 0px)')
+      $('.slideDown').css('display', 'block')
+    } else if ($(window).scrollTop() < $(window).height()) {
+      $('.bgImg').css(
+        'transform',
+        'translate(0px, -' + ($(window).scrollTop() - 150) + 'px)'
+      )
+      $('.goTop').css('display', 'none')
+      $('.slideDown').css('display', 'none')
+    } else {
+      $('.goTop').css('display', 'flex')
     }
+  })
 
-    async function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
+  $('a').on('click', function (e) {
+    if (this.hash != '') {
+      e.preventDefault()
+      const hash = this.hash
+      $('html, body').animate({ scrollTop: $(hash).offset().top }, 800)
     }
+  })
 
-    window.addEventListener('scroll', () => {
-        if ($(window).scrollTop() == '0') {
-            $('.bgImg').css('transform', 'translate(0px, 0px)')
-        } else if ($(window).scrollTop() < $(window).height()) {
-            $('.bgImg').css('transform', 'translate(0px, -' + ($(window).scrollTop() - 150) + 'px)')
-        }
-    });
-});
+  $('.goTop').on('click', function (e) {
+    e.preventDefault()
+    $('html, body').animate({ scrollTop: 0 }, 800)
+  })
+})
